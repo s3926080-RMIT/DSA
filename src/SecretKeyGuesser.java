50e7f09c89b44c2ab2d8caa12897e49aa5311a2d
Tran Viet Hoang <95412389+Tran-Viet-Hoang@users.noreply.github.com>
2023-12-16 21:51:10 +0700

    Update SecretKeyGuesser.java
    add special cases

--- src/SecretKeyGuesser.java
+++ src/SecretKeyGuesser.java
@@ -1,67 +1,111 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SecretKeyGuesser {
-    private static final char[] CHARACTERS = {'M', 'O', 'C', 'H', 'A'};
-    private static final int KEY_LENGTH = 12;
-    private char[] guess = new char[KEY_LENGTH];
-    private int[] matchCounts = new int[KEY_LENGTH];
-
-    public void start() {
-        SecretKey secretKey = new SecretKey();
-        int totalMatchCount = 0;
-        int guessCount = 0;
-        int currentMatchCount = 0;
-
-        Set<String> previousGuesses = new HashSet<>();
+  public void start() {
+    // brute force key guessing
+    SecretKey key = new SecretKey();
+    String str = "MMMMMMMMMMMM";
+    int matchedChars = key.guess(str);
+    System.out.println("Guessing... " + str);
+    int check = matchedChars;
+    int currentCharIndex = 0;
+    int counter = 0;
     
-        // Initialize the guess array with 'M'
-        Arrays.fill(guess, 'M');
-        
-        totalMatchCount = secretKey.guess(new String(guess));
-        previousGuesses.add(new String(guess));
-    
-    
-        while (totalMatchCount < KEY_LENGTH) {
-            for (int i = 0; i < KEY_LENGTH; i++) {
-                if (matchCounts[i] > 0) {
-                    // Skip this position if we've already found a match here
-                    continue;
-                }
-                int prevMatchCount = totalMatchCount;
-                for (char c : CHARACTERS) {
-                    char originalChar = guess[i];
-                    guess[i] = c;
-                    
-                    String currentGuess = new String(guess);
-
-                    if (previousGuesses.contains(currentGuess)) {
-                        continue;
-                    }
-                    int matchCount = secretKey.guess(new String(guess));
-                    guessCount++;
-                    System.out.println("Guess " + guessCount +  ": " +new String(guess) + ", Matching positions: " + matchCount);
-                    if (matchCount > prevMatchCount) {
-                        totalMatchCount = matchCount;
-                        matchCounts[i]++;
-                        previousGuesses.add(currentGuess);          
-                        break;
-                    } else if(matchCount < prevMatchCount) {
-                        // Revert the guess if it didn't improve the match count
-                        guess[i] = originalChar;
-                       
-                        break;
-                    }
-                }
-            }
+    Set <String> previousGuesses = new HashSet<String>();
+    while (matchedChars != 12) {
+      counter++;
+      while (matchedChars == 0) {
+        int currentCharIndexShiftAll = 0;
+        for (int i = 0; i < str.length(); i++) {
+            str = nextOrder(str, currentCharIndexShiftAll);
+            currentCharIndexShiftAll++;
+            
         }
-    
+        matchedChars = key.guess(str);
+        System.out.println("Guessing... " + str + counter);
+      }
 
-        System.out.println("Secret key: " + new String(guess));
-        System.out.println("Number of guesses: " + guessCount);
+      if (matchedChars == 12) {
+        break;
+      }
+
+      previousGuesses.add(str);
+      
+      if (check - matchedChars == 0) { 
+        str = nextOrder(str, currentCharIndex);
+      }
+
+      else if (check - matchedChars < 0) {
+        currentCharIndex++;
+        str = nextOrder(str, currentCharIndex);
+      }
+
+      else if (check - matchedChars > 0) {
+        str = prevOrder(str, currentCharIndex);
+        currentCharIndex++;
+        matchedChars = key.guess(str);
+        continue;
+      }
+
+      check = matchedChars;
+      System.out.println("Guessing... " + str + counter);
+      matchedChars = key.guess(str);
     }
+    System.out.println("I found the secret key. It is " + str);
+  }
+  
+  static int order(char c) {
+    if (c == 'M') {
+      return 0;
+    } else if (c == 'O') {
+      return 1;
+    } else if (c == 'C') {
+      return 2;
+    } else if (c == 'H') {
+      return 3;
+    } 
+    return 4;
+  }
+
+  static char charOf(int order) {
+    if (order == 0) {
+      return 'M';
+    } else if (order == 1) {
+      return 'O';
+    } else if (order == 2) {
+      return 'C';
+    } else if (order == 3) {
+      return 'H';
+    } 
+    return 'A';
+  }
+
+  // Shift current character forward by 1 according to established order
+  public String nextOrder(String current, int currentCharIndex) {
+      char[] characters = current.toCharArray();
+      if (order(characters[currentCharIndex]) == 4) {
+          characters[currentCharIndex] = 'M';
+      }
+      else {
+          characters[currentCharIndex] = charOf(order(characters[currentCharIndex]) + 1);
+      }
+      return String.valueOf(characters);
+  }
+
+  // Shift the current character backwards by 1 according to established order
+  public String prevOrder(String current, int currentCharIndex) {
+      char[] characters = current.toCharArray();
+      if (order(characters[currentCharIndex]) == 0) {
+          characters[currentCharIndex] = 'A';
+      }
+      else {
+          characters[currentCharIndex] = charOf(order(characters[currentCharIndex]) - 1);
+      }
+      return String.valueOf(characters);
+  }
 }
+
 
 
     
