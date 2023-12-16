c41d793f037df36d23e832ee2088d5c1cd9b95da
Tran Viet Hoang <95412389+Tran-Viet-Hoang@users.noreply.github.com>
2023-12-16 21:50:37 +0700

    Update SecretKey.java

--- src/SecretKey.java
+++ src/SecretKey.java
@@ -1,32 +1,36 @@
-import java.util.Random;
 public class SecretKey {
-    private final String key;
-    public char[] getKey;
+  private String correctKey;
+  private int counter;
 
-    public SecretKey() {
-        StringBuilder builder = new StringBuilder();
-        Random random = new Random();
-        String characters = "MOCHA";
-        for (int i = 0; i < 12; i++) {
-            builder.append(characters.charAt(random.nextInt(characters.length())));
-        }
-        this.key = builder.toString();
-    }
+  public SecretKey() {
+    // for the real test, your program will not know this
+    correctKey = "CCCCCCAAAAAA"; 
+    counter = 0;
+  }
 
-    public int guess(String guess) {
-        if (guess.length() != 12) {
-            return -1;
-        }
-        int count = 0;
-        for (int i = 0; i < 12; i++) {
-            if (key.charAt(i) == guess.charAt(i)) {
-                count++;
-            }
-        }
-        return count;
+  public int guess(String guessedKey) {
+    counter++;
+    // validation
+    if (guessedKey.length() != correctKey.length()) {
+      return -1;
     }
+    int matched = 0;
+    for (int i = 0; i < guessedKey.length(); i++) {
+      char c = guessedKey.charAt(i);
+      if (c != 'M' && c != 'O' && c != 'C' && c != 'H' && c != 'A') {  
+       return -1;
+      }
+      if (c == correctKey.charAt(i)) {
+        matched++;
+      }
+    }
+    if (matched == correctKey.length()) {
+      System.out.println("Number of guesses: " + counter);
+    }
+    return matched;
+  }
 
-    public String getKey() {
-        return key;
-    }
+  public static void main(String[] args) {
+    new SecretKeyGuesser().start();
+  }
 }
